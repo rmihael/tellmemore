291a43750754dc033238fdaa593444625e53654a
Michael Korbakov <rmihael@gmail.com>
2013-05-30 02:53:46 -0700

    Some documentation for UserModel

--- src/main/scala/tellmemore/users.scala
+++ src/main/scala/tellmemore/users.scala
@@ -23,13 +23,31 @@ case class UserModel(userDao: UserDao, transactionManager: PlatformTransactionMa
     userDao.getAllByClientId(clientId)
   }
 
+  /**
+   * This method creates new users.
+   * @note Method expects that parent client of these users is already existing.
+   * @note All new users must belong to the same client
+   * @param users users to create
+   * @return If all users were created successfully then result is Right(<number of created users>). If creating users
+   *         failed due to missing client then result would be Left(<client id>) and no changes in the system state
+   *         are made.
+   */
   def bulkInsert(users: Set[User]): Either[String, Int] = {
     transactional() { txStatus =>
+      // TODO: Add transaction rollback in case of Left answer from DAO
       userDao.bulkInsert(users)
     }
   }
 
-  def insert(user: User) {
+  /**
+   * This method creates new user
+   * @note Method expects that parent client of the user is already existing
+   * @param user user to create
+   * @return If user was created successfully then return value would be Right(1) (for consistency with bulkInsert). If
+   *         creating user failed missing client then return value is Left(<client id>) and no changes in the system
+   *         state are made.
+   */
+  def insert(user: User): Either[String, Int] = {
     bulkInsert(Set(user))
   }
 }
