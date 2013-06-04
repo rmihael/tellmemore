1184ebfe2cef3f219372857a4bc5d63020a5bd8e
Michael Korbakov <rmihael@gmail.com>
2013-06-03 23:48:25 -0700

    Tests stubs

---
+++ src/it/scala/tellmemore/events/EventModelSpec.scala
@@ -0,0 +1,24 @@
+package tellmemore.events
+
+import tellmemore.IntegrationTest
+
+class EventModelSpec extends IntegrationTest {
+  "EventModel" should {
+    "get all user events by id and specified timestamp" in {
+      skipped
+    }
+
+    "get all events by client id" in {
+      skipped
+    }
+
+    "persist events" in {
+      skipped
+    }
+
+    "give all uniqut event names with getEventNames" in {
+      skipped
+    }
+  }
+
+}
---
+++ src/it/scala/tellmemore/userfacts/UserFactModelSpec.scala
@@ -0,0 +1,38 @@
+package tellmemore.userfacts
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.scala_tools.time.Imports._
+
+import tellmemore.clients.ClientModel
+import tellmemore.users.UserModel
+import tellmemore.{UserId, User, Client, IntegrationTest}
+
+class UserFactModelSpec extends IntegrationTest {
+  @Autowired var userModel: UserModel = _
+  @Autowired var clientModel: ClientModel = _
+
+  val client = Client("email1@domain.com", "Test user 1", DateTime.now)
+  val user = User(UserId(client.id, "newuser"), DateTime.now)
+
+  "UserFactModel" should {
+    "persist user fact values" in {
+      skipped
+    }
+
+    "create non-existing facts for client on setForUser" in {
+      skipped
+    }
+
+    "detect inconsistencies in between fact type and value in setForUser" in {
+      skipped
+    }
+
+    "rollback any changes to facts in case if any error happened during inserting values in setForUser" in {
+      skipped
+    }
+
+    "list all client's facts with getUserFactsForClient" in {
+      skipped
+    }
+  }
+}
