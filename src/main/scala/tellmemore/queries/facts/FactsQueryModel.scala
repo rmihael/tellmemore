b66fdf58b102096ffb83576894ffc37293adb34f
Michael Korbakov <rmihael@gmail.com>
2013-06-26 22:13:27 -0700

    Added $gt, $lt, $at handling to JSON queries for user facts

--- src/main/scala/tellmemore/queries/facts/FactsQueryModel.scala
+++ src/main/scala/tellmemore/queries/facts/FactsQueryModel.scala
@@ -7,6 +7,7 @@ import tellmemore.userfacts.{UserFact, UserFactModel}
 import tellmemore.infrastructure.time.TimeProvider
 import tellmemore.queries.Moment
 import tellmemore.queries.facts.FactsQueryAst._
+import org.joda.time.DateTime
 
 case class FactsQueryModel(userFactModel: UserFactModel, timeProvider: TimeProvider) {
   import UserFact._
@@ -51,7 +52,7 @@ case class FactsQueryModel(userFactModel: UserFactModel, timeProvider: TimeProvi
 
 private[facts] case class FactsQueryParser(timeProvider: TimeProvider) {
   def apply(unparsedQuery: JsValue): Either[Seq[JsValue], FactsQueryAst] = {
-    import FactsQueryAst._
+    val now = timeProvider.now
     unparsedQuery match {
       case JsObject(Seq(("$and", JsArray(subqueries)))) if subqueries.size >= 2 =>
         val parsedValues = subqueries map {this(_)}
@@ -59,12 +60,25 @@ private[facts] case class FactsQueryParser(timeProvider: TimeProvider) {
       case JsObject(Seq(("$or", JsArray(subqueries)))) if subqueries.size >= 2 =>
         val parsedValues = subqueries map {this(_)}
         processErrors(parsedValues).right map {FactsQueryAst.OrNode(_)}
-      case JsObject(Seq((factName, JsString(value)))) =>
-        Right(StringEqual(factName, value, Moment.Now(timeProvider.now)))
-      case JsObject(Seq((factName, JsNumber(value)))) =>
-        Right(NumericEqual(factName, value.doubleValue(), Moment.Now(timeProvider.now)))
+      case JsObject(Seq((factName, c))) => parseCondition(factName, c, now)
       case badValue => Left(Seq(badValue))
     }
+  }
+
+  private[this] def parseCondition(factName: String, condition: JsValue, now: DateTime) = condition match {
+    case JsString(value) => Right(StringEqual(factName, value, Moment.Now(now)))
+    case JsObject(Seq(("$eq", JsString(value)))) => Right(StringEqual(factName, value, Moment.Now(now)))
+    case JsNumber(value) => Right(NumericEqual(factName, value.doubleValue(), Moment.Now(now)))
+    case JsObject(Seq(("$eq", JsNumber(value)))) => Right(NumericEqual(factName, value.doubleValue(), Moment.Now(now)))
+    case JsObject(Seq(("$eq", JsNumber(value)), ("$at", JsNumber(moment)))) =>
+      Right(NumericEqual(factName, value.doubleValue(), Moment.Timestamp(new DateTime(moment))))
+    case JsObject(Seq(("$gt", JsNumber(value)))) => Right(NumericGreaterThen(factName, value.doubleValue(), Moment.Now(now)))
+    case JsObject(Seq(("$gt", JsNumber(value)), ("$at", JsNumber(moment)))) =>
+      Right(NumericGreaterThen(factName, value.doubleValue(), Moment.Timestamp(new DateTime(moment))))
+    case JsObject(Seq(("$lt", JsNumber(value)))) => Right(NumericLessThen(factName, value.doubleValue(), Moment.Now(now)))
+    case JsObject(Seq(("$lt", JsNumber(value)), ("$at", JsNumber(moment)))) =>
+      Right(NumericLessThen(factName, value.doubleValue(), Moment.Timestamp(new DateTime(moment))))
+    case badValue => Left(Seq(badValue))
   }
 
   private[this] def processErrors(parsedValues: Seq[Either[Seq[JsValue], FactsQueryAst]]): Either[Seq[JsValue], Seq[FactsQueryAst]] =
